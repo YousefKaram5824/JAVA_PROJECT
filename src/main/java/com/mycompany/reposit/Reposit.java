2c1e83cf6a3ac576e9ee02ef538b8340d8df78cf
youssef karam <1020118@stemegypt.edu.eg>
2024-11-21 17:15:26 +0200

    yes

---
+++ src/main/java/com/mycompany/reposit/Reposit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.reposit;
+
+/**
+ *
+ * @author ALRMLY
+ */
+public class Reposit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
